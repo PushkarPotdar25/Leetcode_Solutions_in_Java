f5d5277eee79f828554f27dbcd85ace0fdec94d6
Pushkar Potdar <147264711+PushkarPotdar25@users.noreply.github.com>
2026-02-05 16:56:11 +0530

    Time: 1 ms (98.72%) | Memory: 46.6 MB (71.79%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int[] result = new int[nums.length];
+        int n = nums.length;
+        for(int i=0;i<nums.length;i++){
+            int move = ((i + nums[i]) % n + n) % n;
+            result[i] = nums[move];
+        }
+        return result;
+    }
+}
