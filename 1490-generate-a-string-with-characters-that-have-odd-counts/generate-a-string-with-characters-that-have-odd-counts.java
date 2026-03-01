86f3350d9e1bdf63d98c8dac43076be3316e8426
Pushkar Potdar <147264711+PushkarPotdar25@users.noreply.github.com>
2026-03-01 09:58:01 +0530

    Time: 1 ms (91.17%) | Memory: 42.8 MB (81.45%) - LeetSync

---
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String generateTheString(int n) {
+        StringBuilder ans = new StringBuilder();
+        if(n % 2 == 0){
+            for(int i=0;i<n-1;i++){
+                ans.append('a');
+            }
+            ans.append('b');
+        }
+        else{
+            for(int i=0;i<n;i++){
+                ans.append('a');
+            }
+        }
+        return ans.toString();
+    }
+}
