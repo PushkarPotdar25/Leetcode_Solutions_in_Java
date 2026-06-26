4e0770c67f2bb962c4e03c511851dd329f7c27ea
Pushkar Potdar <147264711+PushkarPotdar25@users.noreply.github.com>
2026-06-26 20:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (14.51%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,26 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
 
-        ans.addAll(inorderTraversal(root.left));
-        ans.add(root.val);
-        ans.addAll(inorderTraversal(root.right));
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+
+        TreeNode curr = root;
+
+        while(curr != null || !st.isEmpty()){
+
+            // Go to the leftmost node
+            while(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }
+
+            // Left subtree finished
+            curr = st.pop();
+
+            // Visit the current node
+            ans.add(curr.val);
+
+            // Now move to the right subtree
+            curr = curr.right;
+        }
 
         return ans;
     }
